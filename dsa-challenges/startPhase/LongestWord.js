28973894e6474ad3ff7bd52fa7d5790166717317
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2024-06-14 09:53:00 -0500

    dsa first challenge longest word

---
+++ dsa-challenges/startPhase/LongestWord.js
@@ -0,0 +1,34 @@
+//'hola mundo' retorna mundo
+
+// ['hola', 'mundo'] 
+
+// 1.variable asigna el mayor y revisa condicion cual es mayor
+// 2.other idea: ordenar array de menor a mayor en longitud
+// 3.pointers
+// 4.recursion
+
+//  4        5        regresa el mayor en long
+
+
+// edge cases - vacio, igual longitud, numeros
+
+// idea 1
+
+function longestWord(phrase) {
+    let words = phrase.split(" "); //O(1)
+    let maxLength = 0; //O(1)
+
+    for (let i = 0; i < words.length -1 ; i++) {  
+        if (words[i].length > words[i+1].length) {  //O(n)
+            maxLength = words[i].length 
+        } else {
+            maxLength = words[i+1]
+        }
+    }  
+
+    return maxLength // O(1)
+
+}
+
+let test = longestWord('Hola mundo mango mandarina')
+console.log(test);
