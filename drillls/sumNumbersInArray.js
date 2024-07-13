a92c33d421ba6b42b3305ea9e87b7b0cff2a4679
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2024-07-13 14:43:31 -0500

    sum of numbers drill

---
+++ drillls/sumNumbersInArray.js
@@ -0,0 +1,19 @@
+// find sum of numbers inside an array
+
+const students = [2,3,4,5,5,6];
+let total = 0;
+for ( let student of students ) {
+  total += student;
+}
+
+console.log(total);
+
+let average;
+average1 = Math.round(total / students.length);
+average2 = (total / students.length);
+
+console.log(average1);
+console.log(average2);
+
+
+
