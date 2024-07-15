59877c7059076189cd0d0b9cf6133b5bd4fb2fd2
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2024-07-15 16:47:07 -0500

    Snake game with js and dom manipulation

---
+++ SnakeGameV0/app.js
@@ -0,0 +1,34 @@
+// Htrml elements
+const board = document.getElementById('board');
+const scoreBoard = document.getElementById('score-board');
+const startButton = document.getElementById('start');
+const gameOver = document.getElementById('game-over');
+
+// Game config settings
+const boardSize = 10;
+const gameSpeed = 80;
+const squareTypes = {
+  emptySquare: 0,
+  snakeSquare: 1,
+  foodSquare: 2
+};
+const directions = {
+  ArrowUp: -10,
+  ArrowDown: 10,
+  ArrowRight: 1,
+  ArrowLeft: -1
+};
+
+// Game variables
+let snake;
+let score;
+let direction;
+let boardSquares;
+let emptySquares;
+let moveInterval;
+
+const startGame = () => {
+  setGame();
+};
+
+startButton.addEventListener('click', startGame);
