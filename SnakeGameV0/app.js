981b6d66f70f34d7fe30e769ab58925f30e72225
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2024-07-24 10:59:25 -0500

    draw snake added, change color and initial functionality for startGame function

--- SnakeGameV0/app.js
+++ SnakeGameV0/app.js
@@ -27,15 +27,58 @@ let boardSquares;
 let emptySquares;
 let moveInterval;
 
+const drawSnake = () => {
+  snake.forEach( square => drawSquare(square, 'snakeSquare'));
+};
+
+
+// square indicates position 
+const drawSquare = (square, type) => {
+  const [row, column] = square.split('');
+  boardSquares[row][column] = squareTypes[type];
+  const squareElement = document.getElementById(square);
+  squareElement.setAttribute('class', `square ${type}`);
+
+  if ( type === 'emptySquare') {
+    emptySquares.push(square);
+  } else {
+    if ( emptySquares.indexOf(square) !== -1 ) {
+      emptySquares.splice(emptySquares.indexOf(square), 1);
+    }
+  }
+};
+
+const createBoard = () => {
+  boardSquares.forEach( ( row, rowIndex ) => {
+    row.forEach( ( column, columnIndex)  => {
+      const squareValue = `${rowIndex}${columnIndex}`;
+      const squareElement = document.createElement('div');
+      squareElement.setAttribute('class', 'square emptySquare');
+      squareElement.setAttribute('id', squareValue);
+      board.appendChild(squareElement);
+      emptySquares.push(squareValue);
+    });
+  });
+};
+
 const setGame = () => {
   snake = ['00', '01', '02', '03'];
   score = snake.length;
   direction = 'ArrowRight';
   boardSquares = Array.from(Array(boardSize), () => new Array(boardSize).fill(squareTypes.emptySquare));
+  console.log(boardSquares);
+  //console.log(Array.from(Array(10))); test
+  board.innerHTML = '';
+  emptySquares = [];
+  createBoard();
 };
 
 const startGame = () => {
   setGame();
+  gameOver.style.display = 'none';
+  startButton.disabled = true;
+  drawSnake();
+
 };
 
 startButton.addEventListener('click', startGame);
