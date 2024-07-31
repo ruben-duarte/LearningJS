d95fe3f8be6cf0a3fdc2fe1da6faccbb060ab498
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2024-07-31 15:33:33 -0500

    arrays and sets exploration

--- core-language/DataStructures.js
+++ core-language/DataStructures.js
@@ -6,6 +6,8 @@
 // Diferent tasks require different dataStructures
 
 // Ordered List of Data - duplicates allowed
+// ==============ArrAYS
+console.log('==arrays')
 let myArray = [ 1, 5, 7, 9];
 myArray[3]; // insertion order is kept, access via index
 //size dinamically adjust, duplicates allowed, mix types ok
@@ -22,10 +24,33 @@ names.push('mitsubishi');
 const toyotaIndex = names.findIndex(element => element === 'toyota');
 console.log(names, ` Toyota index ${toyotaIndex}`)
 
+names.splice(2,1);
+console.log(names);
 
+// ========Sets
 // Unordered List of Data - no duplicates allowed
 let mySet = new Set();
 mySet.add('item1');
+// Insertion order is not stored, element access and extraction via method
+// iterable, size dinamyc, duplicates value not allowed
+// Deletion and finding elements fast
+
+const ids = new Set();
+
+ids.add('hi');
+ids.add(2);
+ids.add('hola');
+ids.add(7);
+
+console.log('==sets')
+for (const item of ids) {
+  console.log(item);
+}
+
+ids.has('hola');
+ids.delete(2);
+console.log(ids);
+
 
 // Key value pairs of Data - Unordered allowed
 let myObject = {
