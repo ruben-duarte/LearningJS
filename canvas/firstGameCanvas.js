5c7f90d5e6331903d86e92a0c66d1f1c4b7978d6
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2024-08-15 17:34:38 -0500

    function update Entity more general that recieve any kind of object as parameter

--- canvas/firstGameCanvas.js
+++ canvas/firstGameCanvas.js
@@ -1,15 +1,4 @@
 let box = document.getElementById("box").getContext("2d");
-//box.fillText('Hello',50,50);
-
-// modify settings
-//box.font = '60px Arial';
-//box.fillStyle = 'blue';
-//box.globalAlpha = 0.5;
-
-// Draw
-//box.fillText('Hola',80,60);
-//box.fillRect(60,60,100,100);
-//box.clearRect(75,75,100,100);
 
 box.font = '30px Arial';
 
@@ -28,47 +17,44 @@ const player = {
 
 //enemy
 const enemy = {
- enemy_coordinateX : 150,
- enemy_speedX : 10,
- enemy_coordinateY : 340,
- enemy_speedY : 15,
- enemy_symbol : 'E'
+ coordinateX : 150,
+ speedX : 10,
+ coordinateY : 340,
+ speedY : 15,
+ symbol : 'E'
 };
 
-function updatePlayer() {
- //player
- player.coordinateX += player.speedX;
- player.coordinateY += player.speedY;
- box.fillText(player.symbol, player.coordinateX, player.coordinateY)
- if ( player.oordinateX < 0 || player.coordinateX > WIDTH ) {
-   console.log(message);
-   player.speedX = -player.speedX;
- } else if ( player.coordinateY < 0 ||player.coordinateY > HEIGHT ) {
-   console.log(message);
-   player.speedY = -player.speedY;
- } 
-}
+const player2 = {
+  coordinateX : 230,
+  speedX : 20,
+  coordinateY : 10,
+  speedY : 5,
+  symbol :'F'
+};
 
-function updateEnemy() {
- //enemy
- enemy.enemy_coordinateX += enemy.enemy_speedX;
- enemy.enemy_coordinateY += enemy.enemy_speedY;
- box.fillText(enemy.enemy_symbol, enemy.enemy_coordinateX, enemy.enemy_coordinateY)
- if ( enemy.enemy_coordinateX < 0 || enemy.enemy_coordinateX > WIDTH ) {
-   console.log(message);
-   enemy.enemy_speedX = -enemy.enemy_speedX;
- } else if ( enemy.enemy_coordinateY < 0 || enemy.enemy_coordinateY > HEIGHT ) {
-   console.log(message);
-   enemy.enemy_speedY = -enemy.enemy_speedY;
- } 
-}
+setInterval(update, 40); // 40 ms equals 25 frames per second
+
+function updateEntity(gameCharacter){
+  gameCharacter.coordinateX += gameCharacter.speedX;
+  gameCharacter.coordinateY += gameCharacter.speedY;
+  box.fillText(gameCharacter.symbol, gameCharacter.coordinateX, gameCharacter.coordinateY)
+
+  if ( gameCharacter.coordinateX < 0 || gameCharacter.coordinateX > WIDTH ) {
+    console.log(message);
+    gameCharacter.speedX = -gameCharacter.speedX;
+  } else if ( gameCharacter.coordinateY < 0 || gameCharacter.coordinateY > HEIGHT ) {
+    console.log(message);
+    gameCharacter.speedY = -gameCharacter.speedY;
+  } 
+ }
 
 function update() {
-  updatePlayer();
-  updateEnemy();
+  updateEntity(player);
+  updateEntity(enemy);
+  updateEntity(player2);
  
 }
 
-setInterval(update, 40); // 40 ms equals 25 frames per second
+
 
 
